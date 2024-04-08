919ad310730a9980ada76f52b5264409473bfacc
Gualberto Colque <gualbertocolque4@gmail.com.com>
2024-04-07 22:57:43 -0400

    lab6

---
+++ src/hw03/q03.js
@@ -0,0 +1,28 @@
+const enlace = document.getElementById('enlace');
+
+enlace.setAttribute('href', 'https://keybr.com');
+
+const buyList = [
+    "books",
+    "bread",
+    "eraser",
+    "earphones",
+    "collection-cards",
+    "hdmi",
+    "vga",
+    "motherboard",
+    "university-books",
+  ];
+  
+  const rootDiv = document.getElementById('root');
+  
+  const ul = document.createElement('ul');
+  
+  buyList.forEach (item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    ul.appendChild(li);
+  });
+
+  rootDiv.appendChild(ul);
+  
